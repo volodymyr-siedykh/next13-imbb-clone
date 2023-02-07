4ce9ba11b2fc94a83395cbd067b320741261c55f
volodymyr-siedykh <datrimius@gmail.com>
2023-02-07 11:14:56 +0200

    v1.0 header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,18 @@
-import './globals.css'
+import Header from '@/components/Header';
+import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
